52bb8b61a807a16c541383d6b5bf760859190153
Alex Singer <alexsinger@google.com>
2017-11-28 01:58:52 -0500

    First commit

---
+++ appcenterdemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appcenterdemo
+//
+//  Created by Alex Singer on 11/28/17.
+//  Copyright © 2017 Alex Singer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
